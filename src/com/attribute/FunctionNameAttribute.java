1dd8a0d4aea11e5518103075f505c064ae894116
Andrew Wilder <andrew.m.wilder@gmail.com>
2014-10-24 19:00:05 -0400

    Adding function name attributes class

---
+++ src/com/attribute/FunctionNameAttribute.java
@@ -0,0 +1,42 @@
+package com.attribute;
+
+import java.util.ArrayList;
+
+public class FunctionNameAttribute {
+	
+	// Enums for return type and param types
+	public enum ReturnType {
+		Void,
+		Int,
+		FixPt
+	};
+	public enum ParamType {
+		Int,
+		FixPt
+	};
+	
+	// Attributes for function name
+	private String functionName;
+	private ReturnType returnType;
+	private ArrayList<ParamType> params;
+	
+	// Getters and setters. You know, because "encapsulation"...
+	public String getFunctionName() {
+		return functionName;
+	}
+	public void setFunctionName(String functionName) {
+		this.functionName = functionName;
+	}
+	public ReturnType getReturnType() {
+		return returnType;
+	}
+	public void setReturnType(ReturnType returnType) {
+		this.returnType = returnType;
+	}
+	public ArrayList<ParamType> getParams() {
+		return params;
+	}
+	public void setParams(ArrayList<ParamType> params) {
+		this.params = params;
+	}
+}
