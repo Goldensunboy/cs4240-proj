f87371e79776ecf4582aed3b4de8160238f44b07
Saman <shareghi.saman@gmail.com>
2014-12-07 16:02:51 -0500

    Yet another fix for arrays. JESUS CHRIST

--- src/com/analyzer/RegisterFactory.java
+++ src/com/analyzer/RegisterFactory.java
@@ -130,16 +130,14 @@ public class RegisterFactory {
 			}
 			return registersToPromote;
 		} else if(lhsVariables[0].matches(".*%ai")){
-			registersToPromote.remove(rhsVariables[0]);
-			return registersToPromote;
+			return new Hashtable<>();
 		} else if(rhsVariables[0].matches(".*%af")) {
 			if(registersToPromote.size()>0){
 				registersToPromote.remove(rhsVariables[1]);
 			}
 			return registersToPromote;
 		} else if(rhsVariables[0].matches(".*%ai")) {
-			registersToPromote.remove(rhsVariables[1]);
-			return registersToPromote;
+			return new Hashtable<>();
 		}
 		
 		for(String rhsVariable : rhsVariables) {
